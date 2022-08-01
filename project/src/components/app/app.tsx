8b9bf036ca02fbf17c794e0e1881d097342eaa78
Евгений <foxspb2012@gmail.com>
2022-08-01 17:50:46 +0300

    Выполнено задание "Маршрут по тайным тропам".

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,57 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {AppRoute, AuthorizationStatus} from '../../const';
 import MainScreen from '../../pages/main-screen/main-screen';
+import LoginScreen from '../../pages/login-screen/login-screen';
+import OfferScreen from '../../pages/offer-screen/offer-screen';
+import OfferNotLoggedScreen from '../../pages/offer-not-logged-screen/offer-not-logged-screen';
+import FavoriteScreen from '../../pages/favorite-screen/favorite-screen';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
+import PrivateRoute from '../private-route/private-route';
+import PrivateRouteCheckLogged from '../private-route/private-route-check-logged';
 
 type AppScreenProps = {
   cardCount: number;
 }
 
 function App({cardCount}: AppScreenProps): JSX.Element {
-  return <MainScreen cardCount={cardCount} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<MainScreen cardCount={cardCount}/>}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginScreen/>}
+        />
+        <Route
+          path={AppRoute.Room}
+          element={
+            <PrivateRouteCheckLogged
+              authorizationStatus={AuthorizationStatus.NoAuth}
+              publicElement={<OfferNotLoggedScreen/>}
+              privateElement={<OfferScreen/>}
+            />
+          }
+        />
+        <Route
+          path={'/favorite'}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <FavoriteScreen/>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="*"
+          element={<NotFoundScreen/>}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- project/src/pages/not-found-screen/not-found-screen.tsx
+++ project/src/pages/not-found-screen/not-found-screen.tsx
@@ -1,9 +1,12 @@
 function NotFoundScreen(): JSX.Element {
   return (
     <main className="page__main page__main--index">
-      <section className="cities__places places">
-        <h1>404. Page not found</h1>
-        <a href="/">Вернуться на главную</a>
+      <section className="container not-found">
+        <img className="property__image" style = {{marginTop: 150}} src="img/error.png" width={796} height={548} alt="Error 404" />
+        <div style = {{textAlign: 'center'}}>
+          <h1>Страница не&nbsp;найдена</h1>
+          <a href="/">Вернуться на главную</a>
+        </div>
       </section>
     </main>
   );
