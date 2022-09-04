541d66dfb9eef85f19864f25be84c29f4879712c
Евгений <foxspb2012@gmail.com>
2022-09-04 16:02:20 +0300

    Доработка по замечаниям проверяющего наставника.

--- project/src/components/login/login.tsx
+++ project/src/components/login/login.tsx
@@ -1,13 +1,18 @@
 import type {AuthData} from '../../types/auth';
-import {FormEvent, useRef} from 'react';
+import {FormEvent, useState, ChangeEvent} from 'react';
 import {useAppDispatch} from '../../hooks';
 import {loginAction} from '../../store/api-action';
+import {toast} from 'react-toastify';
+
+const checkEmailPattern = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/;
+const checkPasswordPattern = /^((?=\S*?[A-Za-z])(?=\S*?[0-9]).{1,})\S$/;
 
 function Login(): JSX.Element {
-  const loginRef = useRef<HTMLInputElement | null>(null);
-  const passwordRef = useRef<HTMLInputElement | null>(null);
-
   const dispatch = useAppDispatch();
+  const [loginForm, setLoginForm] = useState({
+    email: '',
+    password: ''
+  });
 
   const onSubmit = (authData: AuthData) => {
     dispatch(loginAction(authData));
@@ -16,27 +21,47 @@ function Login(): JSX.Element {
   const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
 
-    if (loginRef.current !== null && passwordRef.current !== null) {
-      onSubmit({
-        login: loginRef.current.value,
-        password: passwordRef.current.value,
-      });
+    if (!checkEmailPattern.test(loginForm.email)) {
+      toast('Please enter a valid email');
+      return;
     }
+
+    if (!checkPasswordPattern.test(loginForm.password)) {
+      toast('Please enter at least one letter and number');
+      return;
+    }
+
+    onSubmit({
+      login: loginForm.email,
+      password: loginForm.password,
+    });
   };
 
   return (
     <section className="login">
       <h1 className="login__title">Sign in</h1>
-      <form className="login__form form" action="" method="post" onSubmit={handleSubmit}>
+      <form
+        className="login__form form"
+        action=""
+        method="post"
+        onSubmit={handleSubmit}
+      >
         <div className="login__input-wrapper form__input-wrapper">
           <label className="visually-hidden">E-mail</label>
-          <input ref={loginRef} className="login__input form__input" type="email" name="email" placeholder="Email" required/>
+          <input className="login__input form__input" type="email" name="email" placeholder="Email"
+            required value={loginForm.email}
+            onChange={({target}: ChangeEvent<HTMLInputElement>) => {
+              setLoginForm({...loginForm, email: target.value});
+            }}
+          />
         </div>
         <div className="login__input-wrapper form__input-wrapper">
           <label className="visually-hidden">Password</label>
-          <input ref={passwordRef} className="login__input form__input" type="password"
-            name="password" placeholder="Password" title='Minimum one letter and one number. No spaces.'
-            pattern='^(?=.*[A-Za-z])(?!.* )(?=.*\d).{1,}$' required
+          <input className="login__input form__input" type="password" name="password" placeholder="Password"
+            required value={loginForm.password}
+            onChange={({target}: ChangeEvent<HTMLInputElement>) => {
+              setLoginForm({...loginForm, password: target.value});
+            }}
           />
         </div>
         <button className="login__submit form__submit button" type="submit">Sign in</button>
